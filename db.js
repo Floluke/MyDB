12da6d6a84868e20c1cb5281721c85133f61a927
Floluke <85674168+Floluke@users.noreply.github.com>
2024-07-19 10:51:16 +0700

    test db

---
+++ db.js
@@ -0,0 +1,5 @@
+[
+    { stnDropId: "STN-555", stnDropName: "ชยางกูล 40", crData: "2024-05-01", bigBox: 10, smallBox: 10, allBox: 20 },
+    { stnDropId: "STN-444", stnDropName: "ชยางกูล 42", crData: "2024-04-28", bigBox: 5, smallBox: 5, allBox: 10 },
+    { stnDropId: "STN-333", stnDropName: "ชยางกูล 30", crData: "2024-04-27", bigBox: 10, smallBox: 10, allBox: 20 },
+]
